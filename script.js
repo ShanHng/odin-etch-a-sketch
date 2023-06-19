233afe326d7326b18935a14a61a9dd1d7c37843e
ShanHng <hngshanhern@gmail.com>
2023-06-19 22:12:13 +0800

    Modify drawBoxes function to draw boxes dynamically In the previous iteration, each box is given a specific height and width. Using flexbox, we are able to make the boxes take equal sizes regardless of the number of boxes to be drawn by setting a fixed height and width for the outer container.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const body = document.querySelector("body");
+body.style.display = 'flex';
+body.style.justifyContent = 'center';
+
 const container = document.querySelector(".container");
+container.style.cssText = "display: flex; flex-direction: column; height: 960px; width: 960px;"
 
 function drawBoxes() {
     //  create 16 rows 
@@ -6,13 +11,13 @@ function drawBoxes() {
         const row = document.createElement("div");
         row.className = "row";
         row.setAttribute('style', 
-        "display: flex; justify-content: center;")
+        "display: flex; flex: 1 1 0")
 
         // for each row create 16 boxes
         for (let j = 0; j < 16; j++) {
             const box = document.createElement("div");
             box.classList.add("box");
-            box.style.cssText = "width: 8vh; height: 8vh; border: 1px solid black";
+            box.style.cssText = "box-sizing: box-content; border: 1px solid black; flex: 1 1 0";
             
             // code to make boxes turn blue upon hover
             box.addEventListener('mouseover', e => box.style['background-color'] = 'skyblue');
